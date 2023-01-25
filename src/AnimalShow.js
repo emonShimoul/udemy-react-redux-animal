33e57ff6dff34b7fb442d96188e9b65218107fc5
emonShimoul <emonshimoul@gmail.com>
2023-01-26 03:04:59 +0600

    use the map function

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const AnimalShow = () => {
+const AnimalShow = ({ type }) => {
     return (
         <div>
-            Cow!
+            {type}
         </div>
     );
 };
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import AnimalShow from './AnimalShow';
 import './App.css';
 
 function getRandomAnimal() {
@@ -15,10 +16,19 @@ function App() {
     setAnimals([...animals, getRandomAnimal()]);
   }
 
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index}></AnimalShow>
+  });
+
   return (
     <div className="App">
       <button onClick={handleClick}>Add Animal</button>
-      <div>{animals}</div>
+      <div>{renderedAnimals}</div>
+      {/* <div>
+        {animals.map((animal, index) => {
+          return <AnimalShow type={animal} key={index}></AnimalShow>
+        })}
+      </div> */}
     </div>
   );
 }
